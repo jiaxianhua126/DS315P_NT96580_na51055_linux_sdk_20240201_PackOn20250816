d36c0a05b43d32555e8f09d83906035c72c6c975
zhangdelai <zhangdelai@haotek.com>
2025-10-31 17:14:45 +0800

    4KP25增加关闭HDR .

--- code/application/source/cardv/SrcCode/PrjCfg_DVCam.h
+++ code/application/source/cardv/SrcCode/PrjCfg_DVCam.h
@@ -47,7 +47,7 @@
 
 #define FW_VERSION_RELEASE              DISABLE //DISABLE
 #define FW_CUSTOMER_MODEL               "DS315P"
-#define FW_CUSTOMER_VERSION_NUM         "V0.10"
+#define FW_CUSTOMER_VERSION_NUM         "V0.20"
 
 
 //..............................................................................
