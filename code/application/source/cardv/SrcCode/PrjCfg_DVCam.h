5430497fdc19735a26a02f9433e6030ce473d7c3
zhangdelai <zhangdelai@haotek.com>
2025-11-08 17:42:38 +0800

    V0.40_20251108

--- code/application/source/cardv/SrcCode/PrjCfg_DVCam.h
+++ code/application/source/cardv/SrcCode/PrjCfg_DVCam.h
@@ -47,7 +47,7 @@
 
 #define FW_VERSION_RELEASE              DISABLE //DISABLE
 #define FW_CUSTOMER_MODEL               "DS315P"
-#define FW_CUSTOMER_VERSION_NUM         "V0.35"
+#define FW_CUSTOMER_VERSION_NUM         "V0.40_RZW"
 
 
 //..............................................................................
