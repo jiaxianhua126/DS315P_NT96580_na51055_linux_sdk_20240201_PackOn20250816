7a974c2b0d695a7922f78c9fe86164ce3196edfe
george <george@haotek.com>
2026-04-08 16:41:15 +0800

    UIFlowWifiWait LVGL
    Signed-off-by: george <george@haotek.com>

--- code/application/source/cardv/SrcCode/UIWnd/LVGL_SPORTCAM/UIFlowLVGL/UIFlowWifiWait/UIFlowWifiWait.h
+++ code/application/source/cardv/SrcCode/UIWnd/LVGL_SPORTCAM/UIFlowLVGL/UIFlowWifiWait/UIFlowWifiWait.h
@@ -18,11 +18,11 @@ void UIFlowWifiWaitEventCallback(lv_obj_t* obj, lv_event_t event);
 
 
 extern lv_obj_t* container_1_scr_uiflowwifiwait;
+extern lv_obj_t* image_wifi_scr_uiflowwifiwait;
 extern lv_obj_t* spinner_1_scr_uiflowwifiwait;
 extern lv_obj_t* container_2_scr_uiflowwifiwait;
 extern lv_obj_t* image_cap_scr_uiflowwifiwait;
 extern lv_obj_t* image_con_scr_uiflowwifiwait;
-extern lv_obj_t* image_wifi_scr_uiflowwifiwait;
 
 /**********************
  * GLOBAL PROTOTYPES
