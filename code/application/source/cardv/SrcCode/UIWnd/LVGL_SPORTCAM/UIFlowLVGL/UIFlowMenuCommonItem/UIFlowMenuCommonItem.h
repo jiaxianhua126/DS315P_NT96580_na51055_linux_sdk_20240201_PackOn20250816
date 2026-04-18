65fcad0260f55b6d63f4e17e998fdb9820257480
george <george@haotek.com>
2026-04-18 14:51:21 +0800

    Menu Image
    Signed-off-by: george <george@haotek.com>

--- code/application/source/cardv/SrcCode/UIWnd/LVGL_SPORTCAM/UIFlowLVGL/UIFlowMenuCommonItem/UIFlowMenuCommonItem.h
+++ code/application/source/cardv/SrcCode/UIWnd/LVGL_SPORTCAM/UIFlowLVGL/UIFlowMenuCommonItem/UIFlowMenuCommonItem.h
@@ -19,13 +19,9 @@ void UIFlowMenuCommonItemEventCallback(lv_obj_t* obj, lv_event_t event);
 
 extern lv_obj_t* container_main_menu_scr_uiflowmenucommonitem;
 extern lv_obj_t* button_1_scr_uiflowmenucommonitem;
-extern lv_obj_t* label_1_scr_uiflowmenucommonitem;
 extern lv_obj_t* button_2_scr_uiflowmenucommonitem;
-extern lv_obj_t* label_2_scr_uiflowmenucommonitem;
 extern lv_obj_t* button_3_scr_uiflowmenucommonitem;
-extern lv_obj_t* label_3_scr_uiflowmenucommonitem;
 extern lv_obj_t* button_4_scr_uiflowmenucommonitem;
-extern lv_obj_t* label_4_scr_uiflowmenucommonitem;
 extern lv_obj_t* container_1_scr_uiflowmenucommonitem;
 extern lv_obj_t* container_bottom_bar_scr_uiflowmenucommonitem;
 extern lv_obj_t* image_hint_bar_scr_uiflowmenucommonitem;
@@ -37,6 +33,14 @@ extern lv_obj_t* image_bottom_closelcd_scr_uiflowmenucommonitem;
 extern lv_obj_t* image_title_bar_scr_uiflowmenucommonitem;
 extern lv_obj_t* label_menu_title_scr_uiflowmenucommonitem;
 extern lv_obj_t* label_menu_page_num_scr_uiflowmenucommonitem;
+extern lv_obj_t* image_1_scr_uiflowmenucommonitem;
+extern lv_obj_t* image_2_scr_uiflowmenucommonitem;
+extern lv_obj_t* image_3_scr_uiflowmenucommonitem;
+extern lv_obj_t* image_4_scr_uiflowmenucommonitem;
+extern lv_obj_t* label_4_scr_uiflowmenucommonitem;
+extern lv_obj_t* label_1_scr_uiflowmenucommonitem;
+extern lv_obj_t* label_2_scr_uiflowmenucommonitem;
+extern lv_obj_t* label_3_scr_uiflowmenucommonitem;
 
 /**********************
  * GLOBAL PROTOTYPES
